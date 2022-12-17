d518bb4f091ba1771d605ec87a94ca50e8ff9192
Yifan Shi <stevenls1118@msn.com>
2022-12-17 01:35:28 -0700

    Move to correct folder

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+let cohortName = process.argv[2];
+let limit = Number(process.argv[3]);
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name like '%${cohortName}%'
+LIMIT ${limit};
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.error(`query error`, err.stack));
