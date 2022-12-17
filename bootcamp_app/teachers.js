34a65b6323afb5689388bd860dff60235c8ee0de
Yifan Shi <stevenls1118@msn.com>
2022-12-17 16:36:02 -0700

    Modify students.js and teachers.js to prevent SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name like '%${cohortName}%'
+WHERE cohorts.name LIKE $1 
 ORDER BY teachers.name;
-`)
+`, [`%${cohortName}%`])
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
